b73e0f4e575e7bcc8e599b380621e3a164d22a44
Adam Braden <adambr@microsoft.com>
2020-08-14 12:51:20 -0700

    Initialize

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+            Console.WriteLine("Hello World!");
+            var x = new SimpleMathCppWinRT.SimpleMath();
+
+        }
+    }
+}
